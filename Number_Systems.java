06452b42e0eb12368296e323b6c5c8271d5c8871
cmay4-lab <cmay4@bucs.fsw.edu>
2024-09-19 13:33:46 -0400

    I got "validateIfBinary" made and I did some extra stuff in "main"

--- Number_Systems.java
+++ Number_Systems.java
@@ -3,26 +3,53 @@ import java.lang.Math; // pow() is useful.
 
 public class Number_Systems
 {
-    public static double convertBinaryToDecimal(String number)
-    {
-        int num_of_digits = number.length(); // Declare variable for length.
+    static Scanner input = new Scanner(System.in); // Declare scanner to obtain input.
+    
+    public static double convertBinaryToDecimal(String binary_number) {
         double decimal_value = 0; // Initialize decimal value to equal zero.
 
-        for (int i = 0; i < num_of_digits; i++)
-        {
-            int temp_digit = number.charAt(num_of_digits - i - 1); // Cycle through individual digits of binary number.
-            double temp_part = temp_digit*Math.pow(2,i); // Times then by corresponding power of two.
-            decimal_value = decimal_value + temp_part; // Sum up each converted value into decimal value.
+        for (int i = 0; i < binary_number.length(); i++) {
+            
+            int temp_digit = binary_number.charAt(i); // Cycle through individual digits of binary number.
+            double temp_part = temp_digit*Math.pow(2,binary_number.length() - i - 1); // Times then by corresponding power of two.
+            decimal_value += temp_part; // Sum up each converted value into decimal value.
         }
 
         return decimal_value; // Return the number in decimal form.
     }
+    
+    public static boolean validateIfBinary(String user_input) {
+        boolean input_is_valid = true;
 
-    public static void main(String[] args)
-    {
-
-
+        if (user_input.length() > 16) {
+            input_is_valid = false;
+            System.out.println("Invalid input! Enter a binary number");
+        } else {
+            int i = 0;
+            while (i < user_input.length() && input_is_valid) {
+                if (user_input.charAt(i) != 1 && user_input.charAt(i) != 0) {
+                    input_is_valid = false;
+                    System.out.println("Invalid input! Enter a binary number");
+                }
+                i++;
+            }
+        }
+        return input_is_valid;
     }
 
+    public static void main(String[] args) {
 
+        System.out.println("Enter a binary number");
+        String user_input = input.next();
+
+        while (!validateIfBinary(user_input)) {
+            user_input = input.next();
+        }
+        
+        double decimal_user_input = convertBinaryToDecimal(user_input);
+        
+        
+        
+
+    }
 }
